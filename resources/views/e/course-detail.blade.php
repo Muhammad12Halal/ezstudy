cf2679a2b1d3bf6f23797e52d6f25bc04ec3a87c
nizam1643 <53549938+nizam1643@users.noreply.github.com>
2022-04-03 16:56:17 +0800

    update 3.2

--- resources/views/e/course-detail.blade.php
+++ resources/views/e/course-detail.blade.php
@@ -253,7 +253,7 @@
                                             @forelse ($data->lectures as $detail)
                                                 <li>
                                                     <div class="curriculum-list-box">
-                                                        <span>Lecture {{ $loop->iteration }}: <br> TITLE: {{ $detail->title }} <br> DESC: {{ $detail->desc }} <br> LINK: <a href="{{ $detail->content }}" target="_blank">URL TO CONTENT</a></span>
+                                                        <span>Lecture {{ $loop->iteration }}: <br> TITLE: {{ $detail->title }} <br> DESC: {{ $detail->desc }} <br> LINK: <a href="{{ $detail->file }}" target="_blank">URL TO CONTENT</a></span>
                                                     </div>
                                                     <span>File Type: {{ Str::upper($detail->content) }}</span>
                                                 </li>
